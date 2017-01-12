fdbd454df6d6db3e987c642937da6ca33673c562
Andrew Fletcher <andrewsfletch@gmail.com>
2017-01-12 20:56:43 +0000

    Added views, controllers and routes for 'homebrew', 'contact', 'about', 'press', 'services', and 'strain collection'

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
     def home
     end
+    
+    def about
+    end
+    
+    def services
+    end
+    
+    def strain_collection
+    end
+    
+    def h_brew
+    end
+    
+    def press
+    end
+    
+    def contact
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
  root to: 'pages#home'
+ get 'about', to: 'pages#about'
+ get 'contact', to: 'pages#contact'
+ get 'homebrew', to: 'pages#homebrew'
+ get 'press', to: 'pages#homebrew'
+ get 'services', to: 'pages#services'
+ get 'strain_collection', to: 'pages#strain_collection'
 end
